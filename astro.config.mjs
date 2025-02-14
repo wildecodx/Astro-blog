5de1a15b2ed52786eaa28e4f024014dbfee5b2bc
Wil <willynaga08@gmail.com>
2025-02-14 10:19:47 +0800

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://blog-decodx.netlify.app/",
+  integrations: [preact()]
+});
