6193369e0eab32937df7dbbb49ce4f94116d781f
Wil <willynaga08@gmail.com>
2025-02-14 10:54:19 +0800

    Revert "commit"
    This reverts commit 5de1a15b2ed52786eaa28e4f024014dbfee5b2bc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
-export default defineConfig({
-  site: "https://blog-decodx.netlify.app/",
-  integrations: [preact()]
-});
+export default defineConfig({});
